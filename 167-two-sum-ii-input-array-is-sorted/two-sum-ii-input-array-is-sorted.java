b9e47e8b5c0c147f34e854c499c3503091e5ed5c
Ankit Kumar Gupta <151037638+ankit-gupta-git@users.noreply.github.com>
2026-02-12 16:01:34 +0530

    Time: 408 ms (5.31%) | Memory: 48.7 MB (21.80%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,17 +1,26 @@
 class Solution {
     public int[] twoSum(int[] arr, int target) {
-        //Two pointer approach
+        // //Two pointer approach
 
-        int i =0 ;
-        int j  = arr.length - 1;
+        // int i =0 ;
+        // int j  = arr.length - 1;
 
-        while(i < j) {
-            if(arr[i] + arr[j] < target) {
-                i++;
-            } else if (arr[i] + arr[j] > target) {
-                j--;
-            } else {
-                return new int[] {i + 1, j + 1};
+        // while(i < j) {
+        //     if(arr[i] + arr[j] < target) {
+        //         i++;
+        //     } else if (arr[i] + arr[j] > target) {
+        //         j--;
+        //     } else {
+        //         return new int[] {i + 1, j + 1};
+        //     }
+        // }
+        // return null;
+
+        for(int i = 0; i <= arr.length - 1; i++) {
+            for(int j = i+1; j < arr.length; j++){ 
+                if(arr[i] + arr[j] == target) {
+                    return new int[] {i+1, j+1};
+                }
             }
         }
         return null;
