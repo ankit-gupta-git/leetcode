640aa8e52bf9b279201e4bd0b8b5a50936e32f86
Ankit Kumar Gupta <151037638+ankit-gupta-git@users.noreply.github.com>
2026-03-04 18:07:19 +0530

    Time: 2 ms (99.10%) | Memory: 44.4 MB (75.81%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,33 +1,36 @@
 class Solution {
-    public boolean isAlphaNumericCharacter(char c) {
-        return (c >= 'a' && c <= 'z') || 
-               (c >= 'A' && c <= 'Z') || 
-               (c >= '0' && c <= '9');
-    }
-
+    // public boolean isAlphaNumericCharacter(char c) {
+    //     return (c >= 'a' && c <= 'z') || 
+    //            (c >= 'A' && c <= 'Z') || 
+    //            (c >= '0' && c <= '9');
+    // }
+    //2 pointers
     public boolean isPalindrome(String s) {
         int i = 0, j = s.length() - 1;
 
         while (i < j) {
+            char left = s.charAt(i);
+            char right = s.charAt(j);
 
-            if (!isAlphaNumericCharacter(s.charAt(i))) {
-                i++;   // just increment
+            if (!Character.isLetterOrDigit(s.charAt(i))) {
+                i++;   
                 continue;
             }
 
-            if (!isAlphaNumericCharacter(s.charAt(j))) {
-                j--;   // just decrement
+            if (!Character.isLetterOrDigit(s.charAt(j))) {
+                j--;   
                 continue;
             }
 
-            if (Character.toLowerCase(s.charAt(i)) 
-                != Character.toLowerCase(s.charAt(j))) {
+            if (Character.toLowerCase(left) 
+                != Character.toLowerCase(right)) {
                 return false;
             }
 
             i++;
             j--;
         }
+
         return true;
     }
 }
