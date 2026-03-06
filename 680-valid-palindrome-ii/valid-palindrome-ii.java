3ed5a6f765555fd363013cddf339b29dc3c8f044
Ankit Kumar Gupta <151037638+ankit-gupta-git@users.noreply.github.com>
2026-03-06 14:17:48 +0530

    Time: 4 ms (99.59%) | Memory: 47.8 MB (68.42%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -1,26 +1,27 @@
 class Solution {
     public boolean isPalindrome(String s, int i, int j) {
         while(i < j) {
-            if(s.charAt(i) == s.charAt(j)) {
+            if(s.charAt(i) != s.charAt(j)) {
+                return false;
+            } else{
                 i++;
                 j--;
             }
-            else
-             return false;
         }
         return true;
     }
+
     public boolean validPalindrome(String s) {
         int i = 0;
         int j = s.length() - 1;
 
         while(i < j) {
-            if(s.charAt(i) == s.charAt(j)) {
+            if(s.charAt(i) != s.charAt(j)) {
+                return isPalindrome(s, i+1, j) || isPalindrome(s, i, j-1);
+            } else {
                 i++;
                 j--;
-            }
-            else
-            return isPalindrome(s, i+1, j) || isPalindrome(s, i, j-1);
+            }   
         }
         return true;
     }
