a5059630d1d4f8780b7ad0adf77a98b0f4c2a7d5
Ankit Kumar Gupta <151037638+ankit-gupta-git@users.noreply.github.com>
2026-04-10 00:39:11 +0530

    Time: 10 ms (84.79%) | Memory: 47.2 MB (42.17%) - LeetSync

---
+++ 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minMoves2(int[] nums) {
+        int n = nums.length;
+        //sort the array
+        Arrays.sort(nums);
+
+        //find the median
+        int median = n % 2 == 0 ? (nums[n / 2] + nums [n / 2 - 1]) / 2 : nums[n / 2];
+        int steps = 0;
+
+        //calculate steps
+        for(int num : nums) {
+            steps += Math.abs(num - median);
+        }
+
+        return steps;
+    }
+}
