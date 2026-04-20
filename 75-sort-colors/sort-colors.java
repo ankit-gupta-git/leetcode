bf481bcc73b51d74f804870a0d4374b8dba33ca5
Ankit Kumar Gupta <151037638+ankit-gupta-git@users.noreply.github.com>
2026-04-20 23:29:08 +0530

    Time: 3 ms (14.38%) | Memory: 43.7 MB (13.03%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,53 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        // int n = nums.length - 1;
+        // int count = 0, count1 = 0, count2 = 0;
+
+        // for (int i = 0 ; i <= n; i++){
+        //     if(nums[i] == 0) count++;
+        //     else if (nums[i] == 1) count1++;
+        //     else count2++;
+        // } 
+
+        // int idx = 0;
+
+        // for(int i = 0; i < count; i++){
+        //     nums[idx++] = 0;
+        // }
+
+        //    for(int i = 0; i < count1; i++){
+        //     nums[idx++] = 1;
+        // }
+
+        //    for(int i = 0; i < count2; i++){
+        //     nums[idx++] = 2;
+        // }
+
+    //     int n = nums.length;
+    //     int mid = 0, high = n-1, low = 0;
+
+    //     while (mid <= high) {
+    //         if(nums[mid] == 0){
+    //             swap(nums, low, mid);
+    //             mid++; low++;
+            
+    //         } else if (nums[mid] == 1) {
+    //             mid++;
+
+    //         } else { //2
+    //             swap(nums, mid, high);
+    //             high--;
+
+    //         }
+    //     }
+    // }
+
+    //     private void swap(int[] nums, int i, int j) {
+    //     int temp = nums[i];
+    //     nums[i] = nums[j];
+    //     nums[j] = temp;
+
+
+     Arrays.sort(nums);
+    }
+}
