122ab24ce61388bee950b1aabcc89a917090c2ba
Ankit Kumar Gupta <151037638+ankit-gupta-git@users.noreply.github.com>
2026-05-07 02:08:25 +0530

    Time: 28 ms (96.71%) | Memory: 59.1 MB (59.84%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,34 +1,43 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        if(nums.length < 3) {
-            return new ArrayList<>();
-        }
+    public void twoSumHelper(int f, int[] nums, List<List<Integer>> res) {
+            int i = f + 1, j = nums.length - 1;
 
-        Set<List<Integer>> result = new HashSet<>();
-        Arrays.sort(nums);
-        for(int i = 0; i < nums.length - 2; i++) {
-            int j = i + 1;
-            int k = nums.length - 1;
-            while(j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                List<Integer> list = new ArrayList<>();
-                if (sum == 0) {
-                    list.add(nums[i]);
-                    list.add(nums[j]);
-                    list.add(nums[k]);
-                    j++;
-                    k--;
-                    result.add(list);
-                }
-                else if (sum > 0) {
-                    k--;
-                }
-                else {
-                    j++;
+            while(i < j) {
+                int sum = nums[f] + nums[i] + nums[j];
+
+                if(sum > 0) {
+                    j--;
+                } else if(sum < 0) {
+                    i++;
+                } else {
+                    res.add(Arrays.asList(nums[f], nums[i], nums[j]));
+                    i++;
+                    j--;
+
+                    while(i < j && nums[i] == nums[i - 1]) {
+                        i++;
+                    }
+
+                    while(i < j && nums[i] == nums[j + 1]) {
+                        j--;
+                    }
                 }
             }
-
         }
-        return new ArrayList<List<Integer>>(result);
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+
+        for(int f = 0; f < nums.length; f++) {
+            if(nums[f] > 0) {
+                break;
+            }
+
+            if(f==0 || nums[f] != nums[f-1]) {
+                twoSumHelper(f, nums, res);
+            }
+        }
+        return res;
     }
 }
