66da1023c5e0122e7685cfafa2a89ef1e945d0b0
Ankit Kumar Gupta <151037638+ankit-gupta-git@users.noreply.github.com>
2026-05-16 00:48:36 +0530

    Time: 1 ms (80.61%) | Memory: 42.3 MB (78.72%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -1,18 +1,48 @@
 class Solution {
-    public boolean isHappy(int n) {
-        HashSet<Integer> hs = new HashSet<>();
-
-        while(!hs.contains(n)) {
-            hs.add(n);
-            int sqrSum = 0;
-            while(n != 0) {
-                int rem = n % 10;
-                sqrSum  += rem * rem;
-                n = n/10;
-            }
-            if(sqrSum == 1) return true;
-            else n =sqrSum;
+    public int sumOfSquareOfDigits(int n) {
+        int sum = 0;
+        while(n > 0) {
+            int dig = n % 10;
+            sum  = sum + (dig * dig);
+            n  = n / 10;
         }
-        return false;
+        return sum;
+    }
+
+    public boolean isHappy(int n) {
+        int slow = n, 
+            fast = n;
+
+        while(fast != 1) {
+            slow = sumOfSquareOfDigits(slow);
+            fast = sumOfSquareOfDigits(sumOfSquareOfDigits(fast));
+
+            if(fast == 1) {
+                return true;
+            }
+
+            if(slow == fast) {
+                return false;
+            }
+        }
+
+        return true;
+
+
+
+        // HashSet<Integer> hs = new HashSet<>();
+
+        // while(!hs.contains(n)) {
+        //     hs.add(n);
+        //     int sqrSum = 0;
+        //     while(n != 0) {
+        //         int rem = n % 10;
+        //         sqrSum  += rem * rem;
+        //         n = n/10;
+        //     }
+        //     if(sqrSum == 1) return true;
+        //     else n =sqrSum;
+        // }
+        // return false;
     }
 }
