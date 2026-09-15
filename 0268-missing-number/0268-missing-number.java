57f38437a9a076ff51720b11a84ea312109fc819
Ankit Kumar Gupta <151037638+ankit-gupta-git@users.noreply.github.com>
2026-09-16 03:57:24 +0530

    Time: 1 ms (28.94%), Space: 47.4 MB (48.77%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,17 +1,42 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        Set<Integer> set = new HashSet<>();
+        // Set<Integer> set = new HashSet<>();
 
-        for(int num : nums) {
-            set.add(num);
+        // for(int num : nums) {
+        //     set.add(num);
+        // }
+
+        // for(int num = 0; num <= nums.length; num++) {
+        //     if(!set.contains(num)) {
+        //         return num;
+        //     }
+        // }
+
+        // return -1;
+
+
+        int i = 0;
+
+        while(i < nums.length) {
+            int corrIdx = nums[i];
+
+            if (corrIdx >= nums.length) {
+                i++;
+            } else if(nums[corrIdx] != nums[i]) {
+                int temp = nums[i];
+                nums[i] = nums[corrIdx];
+                nums[corrIdx] = temp;
+            } else {
+                i++;
+            }
         }
 
-        for(int num = 0; num <= nums.length; num++) {
-            if(!set.contains(num)) {
+        for(int num = 0; num < nums.length; num++) {
+            if(num!= nums[num]) {
                 return num;
             }
         }
 
-        return -1;
+        return nums.length;
     }
 }
